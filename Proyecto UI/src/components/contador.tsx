2a212e1d32efea0bf4ff1e4861a2925fc130a2a9
Ruben <rubenrojomoreno@gmail.com>
2024-07-07 03:58:31 +0200

    Solicitud de cuenta a api

--- Proyecto UI/src/components/contador.tsx
+++ Proyecto UI/src/components/contador.tsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 const Micomponente = () => {
+    const [cuenta, setCuenta] = useState('');
+    const pedirCuenta = async () => {
+        try {
+            const response = await fetch('http://localhost:1900/cuenta');
+            if (!response.ok) {
+                throw new Error('Error de respuesta');
+            }
+            setCuenta(`${response.body}`);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+    useEffect(() => {
+        pedirCuenta();
+    }, []);
+
+
     return (
         <div className="text-white text-4xl bg-cyan-700 p-10 rounded-2xl border-white border-[3px]">
-            Mi componente
+            Mi componente {cuenta}
         </div>
     )
 }

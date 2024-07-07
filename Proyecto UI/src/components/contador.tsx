25e80d88098942da2d4aae59fdb69e0fb44b234d
Ruben <rubenrojomoreno@gmail.com>
2024-07-07 03:50:23 +0200

    Componente Commit

---
+++ Proyecto UI/src/components/contador.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const Micomponente = () => {
+    return (
+        <div className="text-white text-4xl bg-cyan-700 p-10 rounded-2xl border-white border-[3px]">
+            Mi componente
+        </div>
+    )
+}
+
+export default Micomponente;

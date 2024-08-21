5a2b9683d5ec0ff89a3040f4b4182ba8cbdf1e4f
Ruben <rubenrojomoreno@gmail.com>
2024-08-21 14:02:12 +0200

    Roman Done

---
+++ Proyecto UI/src/Roman/decimalToRomanConverter.ts
@@ -0,0 +1,155 @@
+import { ErrorOwn } from "../../BuildingBlocks/errorOwn";
+import { Result } from "../../BuildingBlocks/result";
+import type { ConverterType } from "../../Common/converterType";
+
+export class DecimalToRomanConverter implements ConverterType {
+  convert(integerInput: string): Result<string> {
+    const romanNumber = NumberRomanable.fromString(integerInput);
+
+    if (romanNumber.isFailure()) {
+      return Result.error(romanNumber.getError());
+    }
+
+    return this.convertToRoman(romanNumber.getValue());
+  }
+
+  private convertToRoman(value: NumberRomanable): Result<string> {
+    let result = "";
+
+    result = result.concat(
+      this.convertThousands(value.getThousands()),
+      this.convertHundreds(value.getHundreds()),
+      this.convertTens(value.getTens()),
+      this.convertUnits(value.getUnits())
+    );
+
+    return Result.success(result);
+  }
+
+
+  private convertUnits(value: number): string {
+    switch (value) {
+      case 1:
+        return "I";
+      case 2:
+        return "II";
+      case 3:
+        return "III";
+      case 4:
+        return "IV";
+      case 5:
+        return "V";
+      case 6:
+        return "VI";
+      case 7:
+        return "VII";
+      case 8:
+        return "VIII";
+      case 9:
+        return "IX";
+      default:
+        return "";
+    }
+  }
+  private convertTens(value: number): string {
+    switch (value) {
+      case 10:
+        return "X";
+      case 20:
+        return "XX";
+      case 30:
+        return "XXX";
+      case 40:
+        return "XL";
+      case 50:
+        return "L";
+      case 60:
+        return "LX";
+      case 70:
+        return "LXX";
+      case 80:
+        return "LXXX";
+      case 90:
+        return "XC";
+      default:
+        return "";
+    }
+  }
+  private convertHundreds(value: number): string {
+    switch (value) {
+      case 100:
+        return "C";
+      case 200:
+        return "CC";
+      case 300:
+        return "CCC";
+      case 400:
+        return "CD";
+      case 500:
+        return "D";
+      case 600:
+        return "DC";
+      case 700:
+        return "DCC";
+      case 800:
+        return "DCCC";
+      case 900:
+        return "CM";
+      default:
+        return "";
+    }
+  }
+
+  private convertThousands(value: number): string {
+    switch (value) {
+      case 1000:
+        return "M";
+      case 2000:
+        return "MM";
+      case 3000:
+        return "MMM";
+      default:
+        return "";
+    }
+  }
+}
+
+export class NumberRomanable {
+  private value: number;
+
+  private constructor(input: number) {
+    this.value = input;
+  }
+
+  public static fromString(input: string): Result<NumberRomanable> {
+    if (!Number.isInteger(Number(input))) {
+      return Result.error(
+        new ErrorOwn("Invalid roman number format - not a number")
+      );
+    }
+    const number = Number(input);
+
+    if (number < 1 || number > 3999) {
+      return Result.error(
+        new ErrorOwn("Invalid roman number format - out of range (1-3999)")
+      );
+    }
+
+    return Result.success(new NumberRomanable(number));
+  }
+
+  public getUnits(): number {
+    return this.value % 10;
+  }
+
+  public getTens(): number {
+    return Math.floor((this.value % 100) / 10) * 10;
+  }
+
+  public getHundreds(): number {
+    return Math.floor((this.value % 1000) / 100) * 100;
+  }
+  public getThousands(): number {
+    return Math.floor(this.value / 1000) * 1000;
+  }
+}
---
+++ Proyecto UI/src/Roman/romanToDecimalConverter.ts
@@ -0,0 +1,73 @@
+import { ErrorOwn } from "../../BuildingBlocks/errorOwn";
+import { Result } from "../../BuildingBlocks/result";
+import type { ConverterType } from "../../Common/converterType";
+
+export class RomanToDecimalConverter implements ConverterType {
+  convert(romanNumberInput: string): Result<string> {
+    const romanNumber = RomanNumber.fromString(romanNumberInput);
+
+    if (romanNumber.isFailure()) {
+      return Result.error(romanNumber.getError());
+    }
+
+    return this.convertToDecimal(romanNumber.getValue());
+  }
+
+  private convertToDecimal(value: RomanNumber): Result<string> {
+    let previous = RomanValue.Infinity;
+    let result = 0;
+
+    for (let i = 0; i < value.getLength(); i++) {
+      const current = value.getPosition(i);
+      if (current <= previous) {
+        result += Number(current);
+      }
+
+      if (current > previous) {
+        result -= Number(previous) * 2;
+        result += Number(current);
+      }
+
+      previous = current;
+    }
+
+    return Result.success(result.toString());
+  }
+}
+
+export class RomanNumber {
+  private value: string;
+  private static regex = new RegExp(
+    "^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
+  );
+
+  private constructor(input: string) {
+    this.value = input;
+  }
+
+  public static fromString(input: string): Result<RomanNumber> {
+    if (!this.regex.test(input)) {
+      return Result.error(new ErrorOwn("Invalid roman number format provided"));
+    }
+
+    return Result.success(new RomanNumber(input));
+  }
+  public getLength(): number {
+    return this.value.length;
+  }
+
+  public getPosition(index: number): RomanValue {
+    return RomanValue[this.value.charAt(index) as keyof typeof RomanValue];
+  }
+}
+
+export enum RomanValue {
+  I = 1,
+  V = 5,
+  X = 10,
+  L = 50,
+  C = 100,
+  D = 500,
+  M = 1000,
+  Infinity = Number.MAX_SAFE_INTEGER,
+}

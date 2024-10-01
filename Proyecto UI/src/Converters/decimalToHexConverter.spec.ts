3a5c6a46b48cc6bf219034ae509293ce1723df00
Rubirojo05 <rubenrojomoreno@gmail.com>
2024-10-01 10:28:30 +0200

    test decimalHexadecimal Converter

--- Proyecto UI/src/Converters/decimalToHexConverter.spec.ts
+++ Proyecto UI/src/Converters/decimalToHexConverter.spec.ts
@@ -26,6 +26,30 @@ describe("Hexable", () => {
   });
 });
 
+describe("Hexable", () => {
+  it("should return error when nothing is passed", () => {
+    const input = "";
+    const romanizable = NumberHexable.fromString(input);
+    expect(romanizable.getError().message).toEqual("Formato de número hexadecimal no válido - fuera de rango (0-65535)");
+  });
+});
+
+describe("Hexable", () => {
+  it("should return error when lowcase is passed", () => {
+    const input = "a";
+    const romanizable = NumberHexable.fromString(input);
+    expect(romanizable.getError().message).toEqual("Formato de número hexadecimal no válido - no es un número");
+  });
+});
+
+describe("Hexable", () => {
+  it("should return error when special character is passed", () => {
+    const input = "/";
+    const romanizable = NumberHexable.fromString(input);
+    expect(romanizable.getError().message).toEqual("Formato de número hexadecimal no válido - no es un número");
+  });
+});
+
 describe("decimalToHexadecimalConverter", () => {
   it("should return 1C8 when 456 is passed", () => {
     const input = "456";
@@ -34,18 +58,20 @@ describe("decimalToHexadecimalConverter", () => {
   });
 });
 
-describe("romanizable", () => {
-  it("should return error when nothing is passed", () => {
-    const input = "";
-    const romanizable = NumberHexable.fromString(input);
-    expect(romanizable.getError().message).toEqual("Formato de número hexadecimal no válido - fuera de rango (0-65535)");
+describe("HexadecimaltoDecimalConverter", () => {
+  it("should return 456 when 1C8 is passed", () => {
+    const input = "1C8";
+    const converter = new HexadecimalToDecimalConverter();
+    expect(converter.convert(input).getValue()).toEqual("456");
   });
 });
+
+
 
 // tests de numero menor que 1 [hecho]
 // tests de numero mayor que 65535 [hecho]
 // tests de numero 456 -> 1C8 [hecho]
-// test de un numero hexadecimal vacio
-// test de un numero hexadecimal invalido (caracteres en minuscula)
-// test de un numero hexadecimal invalido (caracteres no permitidos)
-// tests de numero CDLVI -> 456 
+// test de un numero hexadecimal vacio [hecho]
+// test de un numero hexadecimal invalido (caracteres en minuscula) [hecho]
+// test de un numero hexadecimal invalido (caracteres no permitidos) [hecho]
+// tests de numero 1C8 -> 456 [hecho]
